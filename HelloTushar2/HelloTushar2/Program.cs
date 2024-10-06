0c5a2dc1fced95bf1f655980b3a6d14c17097241
TusharBurte <tusharburte5656@gmail.com>
2024-10-06 14:19:15 +0530

    added changes

---
+++ HelloTushar2/HelloTushar2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloTushar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Tushar!");
+        }
+    }
+}
